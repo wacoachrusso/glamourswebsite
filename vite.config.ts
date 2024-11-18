41ffd2782982b94f6e251a81678a1c6c2521b2e3
wacoachrusso <wacoachrusso@gmail.com>
2024-11-17 21:34:40 -0500

    yes or nah

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: '',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: (assetInfo) => {
-          const info = assetInfo.name.split('.');
-          const ext = info[info.length - 1];
-          if (ext === 'css') {
-            return `assets/[name].[ext]`;
-          }
-          return `assets/[name]-[hash].[ext]`;
-        },
-      }
-    }
-  },
-  server: {
-    headers: {
-      'Content-Type': 'application/javascript',
-    }
   }
-});
+})
