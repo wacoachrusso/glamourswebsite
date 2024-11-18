9c3a8dafe3716740aa0a31c77821d45e0b53ce0b
wacoachrusso <wacoachrusso@gmail.com>
2024-11-17 21:29:19 -0500

    uuuu

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,30 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
+    assetsDir: '',
     rollupOptions: {
       output: {
-        manualChunks: undefined,
-        entryFileNames: '[name].[hash].js',
-        chunkFileNames: '[name].[hash].js',
-        assetFileNames: '[name].[hash][extname]'
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.');
+          const ext = info[info.length - 1];
+          if (ext === 'css') {
+            return `assets/[name].[ext]`;
+          }
+          return `assets/[name]-[hash].[ext]`;
+        },
       }
+    }
+  },
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript',
     }
   }
 });
