d8674842b7b0edd5c682ab18660a420eabafdde2
wacoachrusso <wacoachrusso@gmail.com>
2024-11-20 23:24:02 -0500

    maybe working again

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
+  server: {
+    port: 5173 // Ensure the port is explicitly set
   }
-})
+});
